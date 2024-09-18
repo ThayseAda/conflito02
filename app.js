39ab65e59ee3407597ca69d534054cee992bb020
Jorge Junior <jltdsjrdev@hotmail.com>
2024-09-18 18:23:42 -0300

    feat: resolvendo conflito

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 const { greet } = require("./utils");
 
 const name = "Alice";
 console.log(greet(name));
-=======
-const { farewell } = require('./utils');
-const name = 'Alice';
-console.log(farewell(name)); 
->>>>>>> qa
